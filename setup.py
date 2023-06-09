430d7f85f0f799c5c4c2a8769efc58d9fdc29745
Vitaliy Ogarko <vogarko@gmail.com>
2023-06-08 22:41:27 -0700

    Fixed the url.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tomofast-tools',
     version='1.0.0',
-    url='https://github.com/mypackage.git',
+    url='https://github.com/TOMOFAST/Tomofast-tools.git',
     description='Tools for Tomofast-x code',
     packages=find_packages(),
     install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
