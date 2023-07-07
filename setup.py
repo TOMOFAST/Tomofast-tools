4e1ebf9eea16d35cda375622ff54ec85bbf438a1
Vitaliy Ogarko <vogarko@gmail.com>
2023-07-07 03:54:34 -0700

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tomofasttools',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/TOMOFAST/Tomofast-tools.git',
     description='Tools for Tomofast-x code',
     packages=find_packages(),
